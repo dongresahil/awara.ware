13690c00400f921b81cd906579b32fd4c2bc298b
Sahil Dongre <your-email@gmail.com>
2026-06-27 12:13:13 +0530

    Use Railway DB

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,12 @@
+import os
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="#Sahil003366",
-    database="awara_ware"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    port=int(os.getenv("DB_PORT", 3306))
 )
 
 cursor = db.cursor()
